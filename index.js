4fa4d52cc332d3fd62de5f018656717d531dfeb4
James <james.hudgins.work@gmail.com>
2020-08-09 15:20:36 -0400

    add role complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const cTable = require('console.table');
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const validator = require('validator');
 
 // create connection to the AWS RDS database **********************************
 var connection = mysql.createConnection({
@@ -53,48 +54,115 @@ function mainTasker() {
         });
 
 
-    function add() {
-        // ask the user what they want to do
-        inquirer.prompt(
-            {
-                name: "add",
-                type: "list",
-                message: "What do you want to add?",
-                choices: ["A new department", "Add new roles", "Add a new employee", "Forget it, I changed my mind (Go back)."],
+            function add() {
+                // ask the user what they want to do
+                inquirer.prompt(
+                    {
+                        name: "add",
+                        type: "list",
+                        message: "What do you want to add?",
+                        choices: ["A new department", "Add new roles", "Add a new employee", "Forget it, I changed my mind (Go back)."],
+                    }
+                    ).then(function ( { add } ) {
+                    switch (add) {
+                        case "A new department":
+                            newDept();
+                            break;
+                        case "Add new roles":
+                            newRole();
+                            break;
+                        case "Add a new employee":
+                            console.log("Case 3");
+                            break;
+                        case "Forget it, I changed my mind (Go back).":
+                            console.log("Case 4")
+                            // return back the last prompt.
+                            return mainTasker();
+                    }});
             }
-            ).then(function ( { add } ) {
-            switch (add) {
-                case "A new department":
-                    newDept();
-                    break;
-                case "Add new roles":
-                    console.log("Case 2");
-                    break;
-                case "Add a new employee":
-                    console.log("Case 3");
-                    break;
-                case "Forget it, I changed my mind (Go back).":
-                    console.log("Case 4")
-                    // return back the last prompt.
-                    return mainTasker();
-            }});
-    }
-    // add a new department
-    function newDept() {
-        inquirer.prompt(
-            {
-                name: "newDept",
-                message: "Name the new department",
-                type: "input"
-            }
-        ).then(function ({ newDept }) {
-            connection.query(`INSERT INTO department (name) VALUES ('${newDept}')`, function (err, data) {
+            // add a new department
+            function newDept() {
+                inquirer.prompt(
+                    {
+                        name: "newDept",
+                        message: "Name the new department",
+                        type: "input",
+                        validate: userEntry => {
+                            // if user response is not nothing
+                            if(userEntry !== ""){
+                                return true;
+                            }
+                            return "You have to enter some information!"
+                        },
+                        default: ""                     
+                    }
+                ).then(function ({ newDept }) {
+                    connection.query(`INSERT INTO department (name) VALUES ('${newDept}')`, function (err, data) {
+                        if (err) throw err;
+                        console.log('Inserted ' + newDept + ' into row ' + data.insertId)
+                        mainTasker();
+                    })
+                })
+            };
+            // add a new role
+            function newRole() {
+            // placeholder for department array
+            let deptList = [];
+
+            connection.query(`SELECT * FROM department`, function (err, data) {
                 if (err) throw err;
-                console.log('Inserted ' + newDept + ' into row ' + data.insertId)
-                mainTasker();
+                // loop through to find all departments
+                for (let i = 0; i < data.length; i++) { 
+                    deptList.push(data[i].name)
+                }
+                // ask the user for info about the new role
+            inquirer.prompt([
+                {
+                    name: "roleTitle",
+                    message: "What is the title of role?",
+                    type: "input",
+                    validate: userEntry => {
+                        // if user response is not nothing
+                        if(userEntry !== ""){
+                            return true;
+                        }
+                        return "You have to enter some information!"
+                    },
+                    default: ""        
+                },
+                {
+                    name: "roleSalary",
+                    message: "What is the salary of the role? (no , or $ needed)",
+                    type: "input",
+                    validate: userEntry => {
+                        // if user response is a number
+                        if(validator.isNumeric(userEntry)){
+                            return true;
+                        }
+                        return "That's not a valid number!";
+                    },
+                    default: ""
+                },
+                {
+                    name: "roleDept",
+                    message: "What department does this role work for?",
+                    type: "list",
+                    choices: deptList
+                }
+            ]).then(function ({ roleTitle, roleSalary, roleDept }) {
+                // set the id
+                let index = deptList.indexOf(roleDept)
+                // add one the the returned index (SQL doesn't start at 0)
+                index += 1;
+                connection.query(`INSERT INTO role (title, salary, department_id) VALUES ('${roleTitle}', '${roleSalary}', ${index})`, function (err, data) {
+                    if (err) throw err;
+                    console.log('Added ' + roleTitle + ' into row ' + data.insertId)
+                    mainTasker();
+                })
             })
         })
     }
+        
 };
 
 
